7a6d52b4e003ba7adcc7c2495f6768b21d5f7774
Thomas <thsbrown1@gmail.com>
2020-09-10 14:06:15 -0700

    Final script file

--- .github/workflows/script.py
+++ .github/workflows/script.py
@@ -1,10 +1,25 @@
 from unityparser import UnityDocument
 
-# Loading and modifying a config file with a single YAML document
-project_settings_file = './NetworkingConfig.asset'
-doc = UnityDocument.load_yaml(project_settings_file)
-NetworkConfig = doc.entry
-version = NetworkConfig.photonNetworkingVersion
-version += 1;
-NetworkConfig.photonNetworkingVersion = version
-doc.dump_yaml()
+
+def run():
+    network_config_file_path = './NetworkingConfig.asset'
+    unity_document = UnityDocument.load_yaml(network_config_file_path)
+    netconfig_monobehaviour_document = unity_document.entry
+
+    try:
+        version = netconfig_monobehaviour_document.photonNetworkingVersion
+    except AttributeError:
+        print("Failed to find photonNetworkVersion in NetworkConfig.asset")
+        return
+
+    old_networking_version = version
+    version += 1
+    print(str(old_networking_version) + " -> " + str(version))
+    netconfig_monobehaviour_document.photonNetworkingVersion = version
+    # see https://docs.github.com/en/actions/reference/workflow-commands-for-github-actions#setting-an-output-parameter
+    print(f"::set-output name=new-networking-version::{version}")
+    # unity_document.dump_yaml()
+    return
+
+
+run()
